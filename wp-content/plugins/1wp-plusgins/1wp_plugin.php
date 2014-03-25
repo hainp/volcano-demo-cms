5a064686a5bf1712b639ed9b6f7ffaef6b3d2917
Anh Duc <anhducrmk@gmail.com>
2014-03-25 21:13:29 +0700

    add 1wp_plugin

---
+++ wp-content/plugins/1wp-plusgins/1wp_plugin.php
@@ -0,0 +1,21 @@
+<?php
+/* Plugin Name: 1WD Slider
+Plugin URI: http://1stwebdesigner.com/
+Description: Slider Component for WordPress
+Version: 1.0
+Author: Rakhitha Nimesh
+Author URI: http://1stwebdesigner.com/
+License: GPLv2 or later
+*/
+?>
+<h1>
+  <strong>Title : (HOT) Luyen duoc tam tha</strong>
+<h1>
+<br>
+Time : 9h00 - 1/4/2014
+<br>
+<br>
+<b>	Thong tin moi nhat duoc cap nhat khi "Luyen" duoc tap tha</b>
+<br>
+trong thoi gian o tu , vi co nhung bieu hien tot duoc huong khoan hong tam tha ve
+
