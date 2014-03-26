41575bc592d8cd94749b184cfab69c0560d12dbe
duythucbk@gmail.com <duythucbk@gmail.com>
2014-03-26 09:46:46 +0700

    add slider

---
+++ wp-content/plugins/1wd_slider/1wd_slider.php
@@ -0,0 +1,10 @@
+<?php
+/* Plugin Name: 1WD Slider
+Plugin URI: http://1stwebdesigner.com/
+Description: Slider Component for WordPress
+Version: 1.0
+Author: Rakhitha Nimesh
+Author URI: http://1stwebdesigner.com/
+License: GPLv2 or later
+*/
+?>
